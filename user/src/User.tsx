d5b3c4306fefce753284e750747e1519ba23318b
Khrystyna Derenivska <khrisderenivska@gmail.com>
2024-02-07 23:30:55 +0200

    user component

---
+++ user/src/User.tsx
@@ -0,0 +1,12 @@
+import { UserInfo } from "./userInfo";
+
+export default function User() {
+  return (
+    <>
+      <h1>Welcome, user!</h1>
+      <p>Login: </p>
+      <p>Password: </p>
+      <p>Full name: </p>
+    </>
+  );
+}
---
+++ user/src/userInfo.ts
@@ -0,0 +1,5 @@
+export interface UserInfo {
+  login: string;
+  password: string;
+  userFullName: string;
+}
